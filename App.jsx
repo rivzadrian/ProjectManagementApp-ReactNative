81f6d77599d8bd254e722da258cfc12a63c5687c
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-05-15 09:04:38 +0700

    add nativewind

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! hereee</Text>
+      <Text className="bg-red-500">ASyyyiiiiikkkkkkk  ini jsxxxx</Text>
       <StatusBar style="auto" />
     </View>
   );
